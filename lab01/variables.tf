7966f897ce7e4d0b616100f6bedc199f21b93cc5
scbmw017 <47151934+scbmw017@users.noreply.github.com>
2019-01-29 13:32:06 -0500

    Update variables.tf

--- lab01/variables.tf
+++ lab01/variables.tf
@@ -1,23 +1,23 @@
 variable "region" {
- description = "AWS Region where this infrastructure should be deployed"
- default = "us-east-1"
+  description = "AWS Region where this infrastructure should be deployed"
+  default = "us-east-1"
 }
 
 variable "identity" {
- description = "A unique name for your resources"
+  description = "A unique name for your resources"
 }
 
 variable "ami" {
- description = "The Amazon Machine Image for new instances."
- default     = "ami-e24b7d9d"
+  description = "The Amazon Machine Image for new instances."
+  default     = "ami-e24b7d9d"
 }
 
 variable "ingress_cidr" {
- default     = "0.0.0.0/0"
- description = "IP block from which connections to this instance will be made"
+  default     = "0.0.0.0/0"
+  description = "IP block from which connections to this instance will be made"
 }
 
 variable "num_webs" {
- description = "The number of servers to run"
- default     = "1"
+  description = "The number of servers to run"
+  default     = "2"
 }
